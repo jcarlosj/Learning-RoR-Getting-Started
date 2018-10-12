49c3ee9c25475474a552ac0d1ac100898a2342a5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-12 16:13:54 -0500

    Incluye lista blanca de parámetros permitidos a guardar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,11 @@ class ArticlesController < ApplicationController
     end
     def create
         #render plain: params[ :article ] .inspect
-         @article = Article .new( params[ :article ] )      # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
+         @article = Article .new( params .require( :article )        # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
+            .permit(                                        # Parametros fuertes (o permitidos)
+                :title,
+                :text
+            ) )
 
          @article .save                                     # Guarda los datos de la instancia en la base de datos
          redirect_to @article                               # Redirecciona al método 'show'
