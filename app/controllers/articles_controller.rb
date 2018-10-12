bff1d2dc48f76a479ee18fe5709ca7adc611ef0d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-12 16:22:57 -0500

    Refactoriza implementación de lista blanca de parámetros
    Aclaración:
    Se incluye la lista blanca de parámetros en un método privado para
    evitar el acceso a los mismos. Práctica recomendada por seguridad
    e integridad de los parámetros que serán usados en el modelo para
    realizar el registro de los mismos en la base de datos.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,19 @@ class ArticlesController < ApplicationController
     end
     def create
         #render plain: params[ :article ] .inspect
-         @article = Article .new( params .require( :article )        # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
-            .permit(                                        # Parametros fuertes (o permitidos)
-                :title,
-                :text
-            ) )
+         @article = Article .new( article_params )          # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
 
          @article .save                                     # Guarda los datos de la instancia en la base de datos
          redirect_to @article                               # Redirecciona al método 'show'
+    end
+
+    private
+    def article_params
+        params
+           .require( :article )                            # Nombre del recurso requerido que posee los parámetros
+           .permit(                                        # Parametros fuertes (o permitidos)
+               :title,
+               :text
+           )
     end
 end
