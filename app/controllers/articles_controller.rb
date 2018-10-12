1734944a64ad16fc195b9a04ae2f1b005ef7c805
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-12 16:00:36 -0500

    Guarda datos a través del controlador
    Aclaración:
     - El controlador accederá al modelo a través de una instancia del
       mismo, la cual permitirá escribir sobre la base de datos los
       valores de los campos pasados por la vista (formulario) al controlador
     - Sin embargo aún no podemos guardar datos por que Rails requiere
       que se implemente seguridad a dichos parámetros para ser registrados

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
     def new
     end
     def create
-        render plain: params[ :article ] .inspect
+        #render plain: params[ :article ] .inspect
+         @article = Article .new( params[ :article ] )      # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
+
+         @article .save                                     # Guarda los datos de la instancia en la base de datos
+         redirect_to @article                               # Redirecciona al método 'show'
     end
 end
