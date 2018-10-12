9610d71ec5b03540f3d419201fd36669392c97d9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-12 17:59:52 -0500

    Valida campo 'title' formulario 'Nuevo artículo'
    Aclaración:
     - Valida:
       + Obligatoriedad del campo
       + Mínimo de caracteres ingresados sea 5
    
    Despliega errores en la vista 'new' del recurso (o ruta) 'Articles'
     - pluralize() pluraliza una cadena de texto si el valor pasado es > 1
     - '@article.errors.count' Cuenta los errores
     - '@article.errors.full_messages' listado con los mensajes de error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,17 @@ class ArticlesController < ApplicationController
         @article = Article .find( params[ :id ] )
     end
     def new
+        @article = Article .new                             # Crea una instancia de la entidad vacía (nueva)
     end
     def create
         #render plain: params[ :article ] .inspect
          @article = Article .new( article_params )          # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
 
-         @article .save                                     # Guarda los datos de la instancia en la base de datos
-         redirect_to @article                               # Redirecciona al método 'show'
+         if @article .save                                  # Valida y Guarda los datos de la instancia en la base de datos si los datos pasados son validados correctamente por la entidad 'Article'
+             redirect_to @article                           # Redirecciona al método 'show'
+         else
+             render 'new'                                   # Renderizará la vista con la ruta (o recurso) 'new' - Volverá al formulario
+         end
     end
 
     private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    # Valida el campo de título del artículo
+    validates :title,
+        presence: true,
+        length: { minimum: 5 }
 end
