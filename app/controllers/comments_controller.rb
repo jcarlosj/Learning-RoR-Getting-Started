1c31a9e4ee434846c0f0a925a36c96360e952df6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-13 11:41:44 -0500

    Define entrada (o método) para la ruta estándar 'create' de comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+    def create
+        @article = Article .find( params[ :article_id ] )           # Obtiene el articulo con el ID especificado
+        @comment = @article .comments .create( comment_params )     # Crea y guarda el comentario en la base de datos a asociando este con el ID del artículo obtenido
+
+        redirect_to article_path( @article )    # Redirecciona al método 'show'
+    end
+
+    private
+    def comment_params
+        params
+            .require( :comment )                # Nombre del recurso requerido que posee los parámetros
+            .permit(                            # Parametros fuertes (o permitidos)
+                :commenter,
+                :body
+            )
+    end
 end
