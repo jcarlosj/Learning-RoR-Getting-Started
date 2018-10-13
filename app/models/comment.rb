349a5f2e475d0dcd1557de93e44e6d16045eee25
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-13 10:30:04 -0500

    Ejecuta migracion y agrega comentarios

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-  belongs_to :article
+  belongs_to :article           # Configura la asociación de registro activo - 'Article'
 end
--- db/migrate/20181013151610_create_comments.rb
+++ db/migrate/20181013151610_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references :article, foreign_key: true
+      t.references :article, foreign_key: true      # Crea una nueva columna en la tabla con el nombre del modelo proporcionado (:articles) al que le agregará el postfijo '_id'
 
       t.timestamps
     end
