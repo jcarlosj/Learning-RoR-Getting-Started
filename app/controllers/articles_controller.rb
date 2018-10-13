0fbc852825c3941b4dc0e52040a983ef476dea0f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-10-13 09:33:35 -0500

    Define entrada (o método) y vista para la ruta estándar 'edit/update'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
     def new
         @article = Article .new                             # Crea una instancia de la entidad vacía (nueva)
     end
+    def edit
+        @article = Article .find( params[ :id ] )
+    end
     def create
         #render plain: params[ :article ] .inspect
          @article = Article .new( article_params )          # Creamos una instancia de la entidad (en el Modelo) y le pasamos los parámetros
@@ -17,6 +20,15 @@ class ArticlesController < ApplicationController
          else
              render 'new'                                   # Renderizará la vista con la ruta (o recurso) 'new' - Volverá al formulario
          end
+    end
+    def update
+        @article = Article .find( params[ :id ] )
+
+        if @article .update( article_params )               # Valida y Guarda los datos de la instancia en la base de datos si los datos pasados son validados correctamente por la entidad 'Article'
+            redirect_to @article                            # Redirecciona al método 'show'
+        else
+            render 'edit'                                   # Renderizará la vista con la ruta (o recurso) 'edit' - Volverá al formulario de edición
+        end
     end
 
     private
